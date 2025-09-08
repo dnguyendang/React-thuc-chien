08941e7c231c3c65cd8c8d08017226a9fa611d40
dungduongthanhson <dungduongthanhson@gmail.com>
2025-09-08 17:16:46 +0700

    update: setup react router

---
+++ 01-react-vite-starter-master/src/layout.tsx
@@ -0,0 +1,12 @@
+
+
+function Layout() {
+
+  return (
+    <>
+      <div>Hello World</div>
+    </>
+  )
+}
+
+export default Layout
--- 01-react-vite-starter-master/src/main.tsx
+++ 01-react-vite-starter-master/src/main.tsx
@@ -1,10 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import Layout from './layout.tsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+  },
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
