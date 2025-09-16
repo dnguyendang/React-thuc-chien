cc6d407cd0d842ae8e5990d8efdef250bcfb240f
Dangg Dungg <dungduongthanhson@gmail.com>
2025-09-16 22:53:29 +0700

    update: homepage

--- 01-react-vite-starter-master/src/pages/client/home.tsx
+++ 01-react-vite-starter-master/src/pages/client/home.tsx
@@ -1,111 +1,193 @@
-import React from 'react';
-import { Layout, Menu, Checkbox, InputNumber, Button, Row, Col, Card, Rate, GetProp, CheckboxOptionType, Divider } from 'antd';
-import { SearchOutlined } from '@ant-design/icons';
-
-const { Header, Content, Sider } = Layout;
-const { Meta } = Card;
+import { FilterTwoTone, ReloadOutlined } from "@ant-design/icons";
+import { Button, Checkbox, Col, Divider, Form, InputNumber, Pagination, Rate, Row, Tabs } from "antd";
+import '@/styles/homepages.scss'
 
 const HomePage = () => {
+    const [form] = Form.useForm();
 
-    const options: CheckboxOptionType<string>[] = [
-        { label: 'Apple', value: 'Apple' },
-        { label: 'Pear', value: 'Pear' },
-        { label: 'Orange', value: 'Orange' },
-    ];
 
-    const onChange: GetProp<typeof Checkbox.Group, 'onChange'> = (checkedValues) => {
-        console.log('checked = ', checkedValues);
-    };
-
-    const productData = [
+    const items = [
         {
-            title: "Tư Duy Về Tiền Bạc - Những Lựa Chọn Tài Chính Đúng Đắn Và Sáng Suốt Hơn",
-            price: 70000,
-            rating: 4.5,
-            imageUrl: `${import.meta.env.VITE_BACKEND_URL}/images/book/1-5e81d7f66dada42752efb220d7b2956c.jpg}`, // Replace with your actual image URLs
-            sold: "1k"
+            key: '1',
+            label: 'Pho bien',
+            children: <></>,
         },
         {
-            title: "Tư Duy Về Tiền Bạc - Những Lựa Chọn Tài Chính Đúng Đắn Và Sáng Suốt Hơn",
-            price: 70000,
-            rating: 4,
-            imageUrl: "url_to_your_image_2.jpg",
-            sold: "1k"
-        },
-        {
-            title: "Tư Duy Về Tiền Bạc - Những Lựa Chọn Tài Chính Đúng Đắn Và Sáng Suốt Hơn",
-            price: 70000,
-            rating: 5,
-            imageUrl: "url_to_your_image_3.jpg",
-            sold: "1k"
-        },
-        {
-            title: "Tư Duy Về Tiền Bạc - Những Lựa Chọn Tài Chính Đúng Đắn Và Sáng Suốt Hơn",
-            price: 70000,
-            rating: 3.5,
-            imageUrl: "url_to_your_image_4.jpg",
-            sold: "1k"
-        },
-        {
-            title: "Tư Duy Về Tiền Bạc - Những Lựa Chọn Tài Chính Đúng Đắn Và Sáng Suốt Hơn",
-            price: 70000,
-            rating: 4,
-            imageUrl: "url_to_your_image_5.jpg",
-            sold: "1k"
-        },
-        {
-            title: "Tư Duy Về Tiền Bạc - Những Lựa Chọn Tài Chính Đúng Đắn Và Sáng Suốt Hơn",
-            price: 70000,
-            rating: 4.5,
-            imageUrl: "url_to_your_image_6.jpg",
-            sold: "1k"
-        },
-    ];
+            key: '2',
+            label: 'Noi bat',
+            children: <></>,
+        }
+    ]
 
     return (
-        <Layout className="site-layout-background" style={{ padding: '24px 0', backgroundColor: '#fff' }}>
-            <Sider width={200} className="site-layout-background" style={{ backgroundColor: '#fff' }}>
+        <>
+            <div className="homepage-container" style={{ maxWidth: 1440, margin: '0 auto ' }}>
+                <Row gutter={[20, 20]}>
+                    <Col md={4} sm={0} xs={0} style={{ border: "1px solid green" }}>
+                        <div style={{ display: 'flex', justifyContent: "space-between" }}>
+                            <span><FilterTwoTone />Bộ lọc tìm kiếm</span>
+                            <ReloadOutlined title="Reset" onClick={() => form.resetFields()} />
+                        </div>
+                        <Form
+                            // onFinish={onFinish}
+                            form={form}
+                            onValuesChange={(changedValues, values) => handleChangeFilter()}
+                        >
+                            <Form.Item
+                                name="category"
+                                label="Danh mục sản phẩm"
+                                labelCol={{ span: 24 }}
+                            >
+                                <Checkbox.Group>
+                                    <Row>
+                                        <Col span={24}>
+                                            <Checkbox value="A">
+                                                A
+                                            </Checkbox>
+                                        </Col>
+                                        <Col span={24}>
+                                            <Checkbox value="B">
+                                                B
+                                            </Checkbox>
+                                        </Col>
+                                    </Row>
+                                </Checkbox.Group>
+                            </Form.Item>
+                            <Divider />
+                            <Form.Item
+                                label="Khoảng giá"
+                                labelCol={{ span: 24 }}
+                            >
+                                <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                                    <Form.Item name={["range", 'from']}>
+                                        <InputNumber
+                                            name='from'
+                                            min={0}
+                                            placeholder="vnd Từ"
+                                            formatter={(value) => `${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
+                                        />
+                                    </Form.Item>
+                                    <span>-</span>
+                                    <Form.Item name={["range", 'to']}>
+                                        <InputNumber
+                                            name='to'
+                                            min={0}
+                                            placeholder="vnd Đến"
+                                            formatter={(value) => `${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
+                                        />
+                                    </Form.Item>
+                                </div>
+                                <div>
+                                    <Button onClick={() => form.submit()}
+                                        style={{ width: "100%" }} type="primary">Áp dụng</Button>
+                                </div>
+                            </Form.Item>
+                            <Divider />
+                            <Form.Item
+                                label="Đánh giá"
+                                labelCol={{ span: 24 }}
+                            >
+                                <div>
+                                    <Rate value={5} disabled style={{ color: '#ffce3d', fontSize: 10 }} />
+                                    <span className="ant-rate-text"></span>
+                                </div>
+                                <div>
+                                    <Rate value={4} disabled style={{ color: '#ffce3d', fontSize: 10 }} />
+                                    <span className="ant-rate-text">trở lên</span>
+                                </div>
+                                <div>
+                                    <Rate value={3} disabled style={{ color: '#ffce3d', fontSize: 10 }} />
+                                    <span className="ant-rate-text">trở lên</span>
+                                </div>
+                                <div>
+                                    <Rate value={2} disabled style={{ color: '#ffce3d', fontSize: 10 }} />
+                                    <span className="ant-rate-text">trở lên</span>
+                                </div>
+                                <div>
+                                    <Rate value={1} disabled style={{ color: '#ffce3d', fontSize: 10 }} />
+                                    <span className="ant-rate-text">trở lên</span>
+                                </div>
+                            </Form.Item>
+                        </Form>
+                    </Col>
 
-                <Checkbox.Group options={options} defaultValue={['Pear']} onChange={onChange} />
-                <Divider />
-            </Sider>
+                    <Col md={20} sm={24} style={{ border: "1px solid red" }}>
+                        <Row>
+                            <Tabs
+                                defaultActiveKey="1"
+                                items={items}
+                            // onChange={onChange}
+                            />
+                        </Row>
+                        <Row className="customize-row">
+                            <div className="column">
+                                <div className="wrapper">
+                                    <div className="thumbnail">
+                                        <img src="http://localhost:8080/images/book/1-5e81d7f66dada42752efb220d7b2956c.jpg" />
+                                    </div>
+                                    <div className="text">Tu Duy ve Tien Bac</div>
+                                    <div className="price">
+                                        {new Intl.NumberFormat(
+                                            'vi-VN',
+                                            { style: 'currency', currency: 'VND' }
+                                        ).format(100000)}
+                                    </div>
+                                    <div className="rating">
+                                        <Rate value={5} disabled style={{ color: '#ffce3d', fontSize: 10 }} />
+                                        <span>Đã bán 1k</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="column">
+                                <div className="wrapper">
+                                    <div className="thumbnail">
+                                        <img src="http://localhost:8080/images/book/1-5e81d7f66dada42752efb220d7b2956c.jpg" />
+                                    </div>
+                                    <div className="text">Tu Duy ve Tien Bac</div>
+                                    <div className="price">
+                                        {new Intl.NumberFormat(
+                                            'vi-VN',
+                                            { style: 'currency', currency: 'VND' }
+                                        ).format(100000)}
+                                    </div>
+                                    <div className="rating">
+                                        <Rate value={5} disabled style={{ color: '#ffce3d', fontSize: 10 }} />
+                                        <span>Đã bán 1k</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="column">
+                                <div className="wrapper">
+                                    <div className="thumbnail">
+                                        <img src="http://localhost:8080/images/book/1-5e81d7f66dada42752efb220d7b2956c.jpg" />
+                                    </div>
+                                    <div className="text">Tu Duy ve Tien Bac</div>
+                                    <div className="price">
+                                        {new Intl.NumberFormat(
+                                            'vi-VN',
+                                            { style: 'currency', currency: 'VND' }
+                                        ).format(100000)}
+                                    </div>
+                                    <div className="rating">
+                                        <Rate value={5} disabled style={{ color: '#ffce3d', fontSize: 10 }} />
+                                        <span>Đã bán 1k</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </Row>
+                        <Divider />
+                        <Row style={{ display: "flex", justifyContent: "center" }}>
+                            <Pagination
+                                defaultCurrent={6}
+                                total={500}
+                                responsive
+                            />
+                        </Row>
 
-            <Layout style={{ padding: '0 24px 24px', backgroundColor: '#fff' }}>
-                <Header style={{ backgroundColor: '#fff', padding: 0 }}>
-                    <Menu mode="horizontal" defaultSelectedKeys={['1']}>
-                        <Menu.Item key="1">Phổ biến</Menu.Item>
-                        <Menu.Item key="2">Hàng Mới</Menu.Item>
-                        <Menu.Item key="3">Giá Thấp Đến Cao</Menu.Item>
-                        <Menu.Item key="4">Giá Cao Đến Thấp</Menu.Item>
-                    </Menu>
-                </Header>
-                <Content
-                    className="site-layout-background"
-                    style={{
-                        padding: 24,
-                        margin: 0,
-                        minHeight: 280,
-                    }}
-                >
-                    <Row gutter={[16, 24]}>
-                        {productData.map((product, index) => (
-                            <Col xs={24} sm={12} md={8} lg={6} key={index}>
-                                <Card
-                                    hoverable
-                                    style={{ width: 240 }}
-                                    cover={<img alt={product.title} src={product.imageUrl} />}
-                                >
-                                    <Meta title={product.title} />
-                                    <p>{product.price.toLocaleString('vi-VN', { style: 'currency', currency: 'VND' })}</p>
-                                    <Rate disabled defaultValue={product.rating} />
-                                    <p>Đã bán {product.sold}</p>
-                                </Card>
-                            </Col>
-                        ))}
-                    </Row>
-                </Content>
-            </Layout>
-        </Layout>
+                    </Col>
+                </Row>
+            </div>
+        </>
     );
 };
 
